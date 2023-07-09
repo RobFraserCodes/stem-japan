bd65e0d23de13331121c2f896adc3904b52b96c8
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-07-09 15:59:56 +0100

    Update route.ts

--- app/api/submitForm/route.ts
+++ app/api/submitForm/route.ts
@@ -1,29 +1,7 @@
-// pages/api/submitForm.js
-import { client } from '@/lib/sanity';
+import React from 'react'
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { firstName, lastName, email, phoneNumber, message } = req.body;
-
-    // Prepare the document data
-    const document = {
-      _type: 'contactForm',
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      message,
-    };
-
-    // Create the document using the Sanity client
-    try {
-      await client.create(document);
-      res.status(200).json({ message: 'Success' });
-    } catch (error) {
-      console.error('Error submitting the form:', error);
-      res.status(500).json({ message: 'An error occurred' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
+export default function route() {
+  return (
+    <div>route</div>
+  )
 }
